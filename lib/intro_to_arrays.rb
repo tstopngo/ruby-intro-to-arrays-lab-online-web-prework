7571c870d7070bcb0c8721571959f2bdc2b46eda
Tracy Ngo <tngo1206@gmail.com>
2019-02-06 04:40:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -19,10 +19,10 @@ def last_element(array)
 end
 
 def first_element_with_array_methods(array)
-  returns array.first
+  array.first
 end
 
 def last_element_with_array_methods(array)
-  returns array.last
+  array.last
 end
   
