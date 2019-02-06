0ad186cfafcb5403fe1a487e26268bbcf07ae706
Tracy Ngo <tngo1206@gmail.com>
2019-02-06 04:31:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,4 +1,14 @@
 def instantiate_new_array
   my_new_array = []
 end
+
+def array_with_two_elements(element1, element2)
+  my_two_array = [element1, element2]
+end
+
+def first_element(array)
+  return array[0]
+end
+
+def third_element(array)
   
