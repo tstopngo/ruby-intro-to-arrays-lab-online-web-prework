0212395ad5aced413c34a5a54e2634d6c771aa71
Tracy Ngo <tngo1206@gmail.com>
2019-02-06 04:39:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -11,11 +11,11 @@ def first_element(array)
 end
 
 def third_element(array)
-  return array[2]
+   array[2]
 end
 
 def last_element(array)
-  return [-1]
+  array[-1]
 end
 
 def first_element_with_array_methods(array)
